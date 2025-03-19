53220ffc39fb76152112eb1331b7cedbe1472e90
ishimwesarah <ishimwesarah09@gmail.com>
2025-03-19 11:39:08 +0200

    path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/", // Ensure correct routing
+  server: {
+    historyApiFallback: true, // Redirects all unknown routes to index.html
+  },
+});
