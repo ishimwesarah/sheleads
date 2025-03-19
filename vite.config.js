ac9d08fc182bea65f513b6564d3569de2909a6ec
ishimwesarah <ishimwesarah09@gmail.com>
2025-03-19 12:30:08 +0200

    FIX

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure correct routing
+  base: process.env.VITE_BASE_PATH|| "/sheleads", // Ensure correct routing
   server: {
     historyApiFallback: true, // Redirects all unknown routes to index.html
   },
