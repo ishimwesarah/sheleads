aa7175dd970c5d9f50fec257c1ac9ae8cd983672
ishimwesarah <ishimwesarah09@gmail.com>
2025-03-20 14:41:40 +0200

    added link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,18 @@ import React, { useState } from "react";
 import "../styles/Navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaTimes } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { Link, useNavigate } from 'react-router-dom'; // Import Link and useNavigate
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false);
-    const navigate = useNavigate(); // Initialize useNavigate
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
-    const toggleModal = () => {
-        setModalOpen(!modalOpen);
-    };
-
     const handleSearch = () => {
         const searchTerm = document.getElementById("searchInput").value;
-
         if (searchTerm) {
             navigate(`/search?query=${searchTerm}`);
         } else {
@@ -34,14 +28,16 @@ function Navbar() {
             </button>
 
             <div className="navbar-brand">
-                <a href="/" className="nav-link" style={{ textDecoration: "none", color: "inherit" }}>She Leads Finance</a>
+                <Link to="/" className="nav-link" style={{ textDecoration: "none", color: "inherit" }}>
+                    She Leads Finance
+                </Link>
             </div>
 
             <div className={`navbar-menu ${menuOpen ? "active" : ""}`}>
-                <a href="/" className="navbar-item">Home</a>
-                <a href="/mentorship" className="navbar-item">Mentorship</a>
-                <a href="/contact" className="navbar-item">Contact us</a>
-                <a href="/Join" className="navbar-item">Join Us</a>
+                <Link to="/" className="navbar-item">Home</Link>
+                <Link to="/mentorship" className="navbar-item">Mentorship</Link>
+                <Link to="/contact" className="navbar-item">Contact us</Link>
+                <Link to="/join" className="navbar-item">Join Us</Link>
             </div>
 
             {/* Search Bar */}
@@ -49,9 +45,6 @@ function Navbar() {
                 <input type="text" placeholder="Search..." id="searchInput" />
                 <button onClick={handleSearch}>🔍</button>
             </div>
-
-            
-            
         </nav>
     );
 }
