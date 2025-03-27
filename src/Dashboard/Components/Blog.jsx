611bed793bc596f8ec201c17b5d0e7f4a8987174
ishimwesarah <ishimwesarah09@gmail.com>
2025-03-27 11:11:05 +0200

    added blog

--- src/Dashboard/Components/Blog.jsx
+++ src/Dashboard/Components/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/AdminBlog.css";
+import "../Styles/AdminBlog.css";
 
 const AdminBlog = () => {
   const [blogs, setBlogs] = useState([]);
